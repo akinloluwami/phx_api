87af40b7915f34b5eedb8829861cb97c96843499
akinloluwami <akinkunmioye42@gmail.com>
2024-03-04 21:46:31 +0100

    _

--- lib/phx_api_web/router.ex
+++ lib/phx_api_web/router.ex
@@ -5,24 +5,11 @@ defmodule PhxApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/api", PhxApiWeb do
+  scope "/todos", PhxApiWeb do
     pipe_through :api
-  end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:phx_api, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+    get "/", TodosController, :todos
+    get "/:id", TodosController, :show
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: PhxApiWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
   end
 end
