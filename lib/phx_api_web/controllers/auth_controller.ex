0da656448b7917d020ee4ac7ea8202b606c04997
akinloluwami <akinkunmioye42@gmail.com>
2024-03-04 22:13:09 +0100

    auth_

---
+++ lib/phx_api_web/controllers/auth_controller.ex
@@ -0,0 +1,11 @@
+defmodule PhxApiWeb.AuthController do
+  use PhxApiWeb, :controller
+
+
+  def signup(conn, %{username, password}) do
+  end
+
+  def login(conn, %{username, password}) do
+  end
+
+end
