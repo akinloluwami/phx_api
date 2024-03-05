9fbf31af854de1ce2352ccc26e5b25b2d757e608
akinloluwami <akinkunmioye42@gmail.com>
2024-03-05 10:59:23 +0100

    auth

--- lib/phx_api_web/controllers/auth_controller.ex
+++ lib/phx_api_web/controllers/auth_controller.ex
@@ -2,10 +2,26 @@ defmodule PhxApiWeb.AuthController do
   use PhxApiWeb, :controller
 
 
-  def signup(conn, %{username, password}) do
+
+
+  def signup(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_status(:created)
+        |> render("show.json", user: user)
+
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(YourProjectNameWeb.ChangesetView, "error.json", changeset: changeset)
+    end
+
   end
 
-  def login(conn, %{username, password}) do
+  def login(conn) do
   end
 
 end
